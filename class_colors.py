be17347e5e9c4f36aa64aac108387f9936f281cd
Alix Chagué <alix.chague@inria.fr>
2019-03-25 15:07:35 +0100

    default classes

--- class_colors.py
+++ class_colors.py
@@ -1,22 +1,13 @@
 my_classes = {
     # color : BGR, not RGB
     # order : lowest number = lowest layer
-    "illustration": {
-        "color":[0,183,0], # green
+    "text": {
+        "color":[0,255,0], # green
         "order":1},
-    "tableau": {
-        "color":[0,0,210], # red
-        "order":6},
-    "séparateur": {
-        "color": [200, 0, 0], # blue
-        "order":3},
-    "texte": {
-        "color":[0,255,255], # yellow
+    "table": {
+        "color":[255,0,0], # blue
         "order":2},
-    "liste": {
-        "color":[255,230,0],  # turquoise
-        "order":5},
-    "en-tête": {
-        "color":[255,0,155], # purple
-        "order":4}
+    "image": {
+        "color": [0, 0, 255], # rouge
+        "order":3}
 }
